fef8761f8d2b1313ec410452a28dd72961774ce2
Jiri Gloza <gl031k@gmail.com>
2020-06-04 21:29:48 +0200

    First unfinished alpha version of BUlls and Cows program for Engeto Python academy

--- Project2.py
+++ Project2.py
@@ -3,7 +3,7 @@
 # Define basic variables
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 Hello_user = '''
-Hi there user!
+Hi there !
 I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.
 Enter a number'''
@@ -14,27 +14,37 @@ def main():
     print(Hello_user)
     rand_number = list(map(int, random.sample(nums, 4)))
     print(rand_number)
-    user_guess = user_input(rand_number)
-    bulls, cows = bulls_and_cows(user_guess, rand_number)
-    print(f" {bulls} Bulls, {cows} Cows")
+    guess_counter = 0
+    user_review = ""
+    while True:
+        user_guess = user_input(rand_number)
+        bulls, cows = bulls_and_cows(user_guess, rand_number)
+        print(f" {bulls} Bulls, {cows} Cows")
+        guess_counter = guess_counter + 1
+        if bulls == 4:
+            if guess_counter < 2:
+                user_review = "Amazing"
+            elif guess_counter < 10:
+                user_review = "Average"
+            else: user_revies = "Not so good"
+            print(f"Correct, you've guessed the right number in {guess_counter} guesses! \nThats {user_review}")
+            break
 
 
 
 def user_input(rand_number):
     guess_list = []
     while True:
-        guess_num = input(":")
+        guess_num = input(">>> ")
         try:
             if len(str(guess_num)) == 4:
                 for i in str(guess_num):
                     guess_list.append(int(i))
                 return guess_list
-            elif guess_num == "":
-                print("prazdna hodnota")
             else:
-                print("Cislo musi byt 4 mistne")
+                print("zadejte prosim 4 cislice")
         except ValueError:
-            print("Prosim zadejte pouze cislice")
+            print("Spatny format cisla")
 
 
 def bulls_and_cows(guess_num, rand_num):
@@ -44,7 +54,7 @@ def bulls_and_cows(guess_num, rand_num):
         for j, y in enumerate((guess_num)):
             if i == j and x == y:
                 bulls = bulls + 1
-            elif x == y:
+            elif y == x and not j == i:
                 cows = cows + 1
     return bulls, cows
 
